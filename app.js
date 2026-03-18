533b21fdfc0af92aad070084ccf69edb10c2fd4d
vVieira9 <araujocvastds@hotmail.com>
2026-03-18 16:48:03 -0300

    corrigir erros de validação

--- app.js
+++ app.js
@@ -2,18 +2,29 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let numeroInicial = parseInt(document.getElementById('de').value);
     let numeroFinal = parseInt(document.getElementById('ate').value);
-    let numero;
     let sorteados = [];
 
-    
+    if(isNaN(quantidade) || isNaN(numeroInicial) || isNaN(numeroFinal)) {
+        alert('Certifique-se de preencher os campos corretamente.')
+        return;
+    }
+
     if (numeroInicial >= numeroFinal) {
         alert('Campo "Do número" deve ser inferior ao campo "Até o número".');
         alert('Insira as informações corretamente e tente novamente.');
-        reiniciar();''
+        return;
+    }
+    
+    let intervalo = numeroFinal - numeroInicial + 1;
+
+    if (quantidade > intervalo) {
+        alert('O campo "Quantidade" deve ser menor ou igual ao intervalo entre os campos "Do número" e "Até o número".');
         return;
     }
     
     for (let i = 0; i < quantidade; i++) {
+        let numero;
+
         numero = obterNumeroAleatorio(numeroInicial,numeroFinal);
         while (sorteados.includes(numero)){
             numero = obterNumeroAleatorio(numeroInicial,numeroFinal);
@@ -50,6 +61,7 @@ function reiniciar() {
     document.getElementById('quantidade').value = '';
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
+
     document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
     alterarStatusReiniciar();
 }
